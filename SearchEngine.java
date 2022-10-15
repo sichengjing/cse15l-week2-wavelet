1d5f2c5900459d10d5205fadb52cda42134c9dcf
sichengjing <sjing@ucsd.edu>
2022-10-14 21:37:06 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
 
 class SearchEngine {
     public static void main(String[] args) throws IOException {
@@ -12,5 +13,40 @@ class SearchEngine {
         int port = Integer.parseInt(args[0]);
 
         Server.start(port, new QueryHandler());
+    }
+}
+
+class QueryHandler implements URLHandler {
+
+    List<String> s = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Number: %d", s.size());
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if ("s".equals(parameters[0])) {
+                    s.add(parameters[1]);
+                    return String.format("List add %s!", parameters[1]);
+                }
+            }else if (url.getPath().contains("/search")){
+                String[] parameters = url.getQuery().split("=");
+                String returnS = "";
+                if ("s".equals(parameters[0])){
+                    for (String s1 : s) {
+                        if(s1.contains(parameters[1])){
+                            returnS += (" AND "+s1);
+                        }
+                    }
+                }
+                if (returnS.startsWith(" AND ")){
+                    returnS = returnS.substring(5);
+                }
+                return String.format("List is %s!", returnS);
+            }
+            return "404 Not Found!";
+        }
     }
 }
