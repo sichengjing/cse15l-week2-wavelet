f38878cc1e20ca0fd1d01c56db460fd399f37541
sichengjing <sjing@ucsd.edu>
2022-10-14 20:46:11 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,30 +1,16 @@
+import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
-class SearchEngine implements URLHandler {
-    ArrayList<String> arr = new ArrayList();
-    ArrayList<String> ans = new ArrayList();
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
 
-    public void add(URI url){
-        if (url.getPath().contains("/add?")) {
-            String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                arr.add(parameters[1]);
-            }
-        }
-    }
-    
-    public ArrayList search(URI url){
-        if (url.getPath().contains("/search?")){
-            String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                String sub = String.format(parameters[1]);
-            }
-            for(String s : arr){
-                if(s.contains(sub)){
-                    ans.add(s);
-                }
-                return ans;
-            }
-        }
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new QueryHandler());
     }
 }
